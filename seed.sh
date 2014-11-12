f29861de86a58a4991cd2d68593170d2ea142cb3
Kevin Morgan <kevinm416@gmail.com>
2014-11-11 23:38:27 -0800

    Added notes from deploying on digital ocean for the first time.

--- seed.sh
+++ seed.sh
@@ -1,8 +1,15 @@
+# create and run a container named postgres
 docker run \
   --detach \
   --name postgres \
   --publish 127.0.0.1:5432:5432 \
-  stackbrew/postgres:latest 
+  stackbrew/postgres:9.3 
+
+# open sql shell in container
+docker exec \
+  --tty \
+  --interactive \
+  bash
 
 psql \
   -U postgres \
